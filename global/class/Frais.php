0d64dd29d7957d17f33f1067b44a72c3e7c375b0
torrent benoit <torrentdu64@gmail.com>
2017-07-01 12:06:53 +0200

    add atribut for frais

--- global/class/Frais.php
+++ global/class/Frais.php
@@ -8,4 +8,13 @@ class Frais extends Entity {
     private $montant; // change to tel
     private $date; // change to adresse
     private $commentaire;
+
+    private $transport;
+    private $distance;
+    private $gps;
+    private $depart;
+    private $arriver;
+    private $nbNuit;
+    private $nbRepas;
+    private $justificatif;
     }
