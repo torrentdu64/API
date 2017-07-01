b875a0794e7bd4ec70d1e26349ffb1e988947f00
torrent benoit <torrentdu64@gmail.com>
2017-07-01 12:03:04 +0200

    add entity to frais

---
+++ global/class/Frais.php
@@ -0,0 +1,11 @@
+<?php
+
+class Frais extends Entity {
+
+    private $id;
+    private $nature;
+    private $titre;
+    private $montant; // change to tel
+    private $date; // change to adresse
+    private $commentaire;
+    }
