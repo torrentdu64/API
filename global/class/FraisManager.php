5cc8a632798f5bb248fdeb62db585535fad9040a
torrent benoit <torrentdu64@gmail.com>
2017-07-01 15:22:59 +0200

    change pdo prepare to owne db

--- global/class/FraisManager.php
+++ global/class/FraisManager.php
@@ -26,14 +26,14 @@ class FraisManager extends Manager {
     }
 
     public function create(Frais & $frais) {
-        $this->pdoStatement = $this->pdo->prepare("INSERT INTO frais(nom, prenom, genre, dateN, email, photo)"
-                . " VALUES(:nom,:prenom,:genre ,:dateN ,:email ,:photo)");
-        $this->pdoStatement->bindValue(':nom', $frais->getNom(), PDO::PARAM_STR);
-        $this->pdoStatement->bindValue(':prenom', $frais->getPrenom(), PDO::PARAM_STR);
-        $this->pdoStatement->bindValue(':genre', $frais->getGenre(), PDO::PARAM_STR);
-        $this->pdoStatement->bindValue(':dateN', $frais->getDateN(), PDO::PARAM_STR);
-        $this->pdoStatement->bindValue(':email', $frais->getEmail(), PDO::PARAM_STR);
-        $this->pdoStatement->bindValue(':photo', $frais->getPhoto(), PDO::PARAM_STR);
+        $this->pdoStatement = $this->pdo->prepare("INSERT INTO frais(nature, titre, montant, date, commentaire, transport)"
+                . " VALUES(:nature,:titre,:montant ,:date ,:commentaire ,:transport)");
+        $this->pdoStatement->bindValue(':nature', $frais->getNom(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':titre', $frais->getPrenom(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':montant', $frais->getGenre(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':date', $frais->getDateN(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':commentaire', $frais->getEmail(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':transport', $frais->getPhoto(), PDO::PARAM_STR);
         $this->pdoStatement->execute();
 
         $data = $this->pdoStatement->fetch();
@@ -43,15 +43,15 @@ class FraisManager extends Manager {
 
     public function update(Frais & $frais) {
 
-        $this->pdoStatement = $this->pdo->prepare("UPDATE * FROM frais(nom, prenom, genre, dateN, email, photo)"
-                . " VALUES(:nom,:prenom,:genre ,:dateN ,:email ,:photo) WHERE id = :id");
+        $this->pdoStatement = $this->pdo->prepare("UPDATE * FROM frais(nature, titre, montant, date, commentaire, transport)"
+                . " VALUES(:nature,:titre,:montant ,:date ,:commentaire ,:transport) WHERE id = :id");
         $this->pdoStatement->bindValue(':id', $frais->getId(), PDO::PARAM_INT);
-        $this->pdoStatement->bindValue(':nom', $frais->getNom(), PDO::PARAM_STR);
-        $this->pdoStatement->bindValue(':prenom', $frais->getPrenom(), PDO::PARAM_STR);
-        $this->pdoStatement->bindValue(':genre', $frais->getGenre(), PDO::PARAM_STR);
-        $this->pdoStatement->bindValue(':dateN', $frais->getDateN(), PDO::PARAM_STR);
-        $this->pdoStatement->bindValue(':email', $frais->getEmail(), PDO::PARAM_STR);
-        $this->pdoStatement->bindValue(':photo', $frais->getPhoto(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':nature', $frais->getNom(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':titre', $frais->getPrenom(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':montant', $frais->getGenre(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':date', $frais->getDateN(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':commentaire', $frais->getEmail(), PDO::PARAM_STR);
+        $this->pdoStatement->bindValue(':transport', $frais->getPhoto(), PDO::PARAM_STR);
         $this->pdoStatement->execute();
 
         $data = $this->pdoSatement->fetch();
