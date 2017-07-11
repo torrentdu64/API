e5749c9d9fbb7b388f1d930d1e7f81375d87e195
MickaelDuprat <contact@mickaelduprat.fr>
2017-07-11 21:06:10 +0200

    Modification de params.php poiur BDD locale

--- global/params.php
+++ global/params.php
@@ -4,16 +4,16 @@ require_once 'init.php';
 
 // On définit les constantes de connexion à la base de donnée.
 
-define('_dbHost', 'mickaeldul64.mysql.db'); // localhost
+/*define('_dbHost', 'mickaeldul64.mysql.db'); // localhost
 
 define('_dbName', 'mickaeldul64'); // expense_manager
 define('_dbUser', 'mickaeldul64'); // root
-define('_dbPassW', 'Wayspeis64'); // root
+define('_dbPassW', 'Wayspeis64'); // root*/
 
-// define('_dbHost', 'localhost'); // mickaeldul64.mysql.db
-//define('_dbName', 'expense_manager'); // u596614644_dev
-//define('_dbUser', 'root'); // u596614644_user
-//define('_dbPassW', 'root'); // Omb3qCt8]$yUtFN
+define('_dbHost', 'localhost'); // mickaeldul64.mysql.db
+define('_dbName', 'expense_manager'); // u596614644_dev
+define('_dbUser', 'root'); // u596614644_user
+define('_dbPassW', 'root'); // Omb3qCt8]$yUtFN
 
 
 //Définition du répertoire de base.
