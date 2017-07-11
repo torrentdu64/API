8b32f13eabc874b7820254ff536aec026572bfdc
torrent benoit <torrentdu64@gmail.com>
2017-07-11 12:06:21 +0200

    commit after pull

--- global/params.php
+++ global/params.php
@@ -4,19 +4,15 @@ require_once 'init.php';
 
 
 // On définit les constantes de connexion à la base de donnée.
-define('_dbHost', 'localhost');
-
-define('_dbName', 'expense_manager2');
-define('_dbUser', 'root');
-define('_dbPassW', '');
-
-//define('_dbName', 'expense_manager'); // u596614644_dev
-//define('_dbUser', 'root'); // u596614644_user
-//define('_dbPassW', 'root'); // Omb3qCt8]$yUtFN
-
+define('_dbHost', 'mysql.hostinger.fr');
+define('_dbName', 'u596614644_dev');
+define('_dbUser', 'u596614644_user');
+define('_dbPassW', 'Omb3qCt8]$yUtFN');
 
 //Définition du répertoire de base.
-define('_dirRoot', $_SERVER['DOCUMENT_ROOT'] . 'Api/API/global');
+define('_dirRoot', $_SERVER['DOCUMENT_ROOT'] . '/API/global');
 
 //Définition du répertoire des classes.
 define('_dirclass', _dirRoot.'/class/');
+
+
