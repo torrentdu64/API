facf41126c9d1757dc0862cee68c182ec0c99fa3
torrent benoit <torrentdu64@gmail.com>
2017-07-11 16:02:43 +0200

    commit before pull

--- controller.php
+++ controller.php
@@ -1,210 +1,236 @@
 <?php
-header('Content-Type: application/json');//Forcer au fichier php de reconnaître le format Json
-require_once 'global/init.php';
 
 
 
+require_once 'init.php';
 
-//$pdo = new PDO('mysql:host=localhost;dbname=expense_manager2;', 'root', '');
 
+// On définit les constantes de connexion à la base de donnée.
+define('_dbHost', 'mickaeldul64.mysql.db');
 
+define('_dbName', 'mickaeldul64');
+define('_dbUser', 'mickaeldul64');
+define('_dbPassW', 'Wayspeis64');
 
-$requete=$pdo->prepare("SELECT * FROM justificatifs");
-$requete->execute();
+//define('_dbName', 'expense_manager'); // u596614644_dev
+//define('_dbUser', 'root'); // u596614644_user
+//define('_dbPassW', 'root'); // Omb3qCt8]$yUtFN
 
-$retour["success"]=true;
-$retour["message"]="Les justificatifs";
 
+//Définition du répertoire de base.
+define('_dirRoot', $_SERVER['DOCUMENT_ROOT'] . 'API/global');
 
-$retour["results"]["justificatifs"]=$requete->fetchAll();
 
-echo json_encode($retour);//Mettre au format Json    //On a un doublon
+//Définition du répertoire des classes.
+define('_dirclass', _dirRoot.'/class/');
 
 
-                             /*TABLE client*/
+// header('Content-Type: application/json');//Forcer au fichier php de reconnaître le format Json
+// require_once 'global/init.php';
 
-/*$client1 = new Client([
-	//'IdClient' => 144,
-    'NomClient' => 'DUPOND',
-    'PrenomClient' => 'Jean',
-    'Adresse1Client' => '8 rue bon accueil',
-    'Adresse2Client' => 'Appartement 8',
-    'CodePostalClient' => '64140',
-    'VilleClient' => 'BILLERE',
-    'TelephoneBureauClient' => '0559482715',
-    'TelephoneMobileClient' => '0686047638',
-    'MailClient' => 'contact@mickaelduprat.fr',
-    'BudgetMaxRemboursementClient' => 200
-]);*/
 
 
 
+// //$pdo = new PDO('mysql:host=localhost;dbname=expense_manager2;', 'root', '');
 
- /*$client = new ClientManager(); */
 
 
- $client = new ClientManager();
+// $requete=$pdo->prepare("SELECT * FROM justificatifs");
+// $requete->execute();
 
-            /*Ajout*/
+// $retour["success"]=true;
+// $retour["message"]="Les justificatifs";
 
- /*$client->create($client1);*/
 
-        /*Modification*/
+// $retour["results"]["justificatifs"]=$requete->fetchAll();
 
-/*$client1->setPrenomClient("toto");
-$client->update($client1);*/
+// echo json_encode($retour);//Mettre au format Json    //On a un doublon
 
 
+//                              /*TABLE client*/
 
-        /*Suppression*/
+// /*$client1 = new Client([
+// 	//'IdClient' => 144,
+//     'NomClient' => 'DUPOND',
+//     'PrenomClient' => 'Jean',
+//     'Adresse1Client' => '8 rue bon accueil',
+//     'Adresse2Client' => 'Appartement 8',
+//     'CodePostalClient' => '64140',
+//     'VilleClient' => 'BILLERE',
+//     'TelephoneBureauClient' => '0559482715',
+//     'TelephoneMobileClient' => '0686047638',
+//     'MailClient' => 'contact@mickaelduprat.fr',
+//     'BudgetMaxRemboursementClient' => 200
+// ]);*/
 
-// $client->delete($client1);
 
 
 
-                            /*TABLE notedefrais*/
+//  /*$client = new ClientManager(); */
 
-/*$notedefrais1 = new NoteDeFrais([
-    'IdNoteDeFrais' => 101,
-    'IntituleNDF' => 'RDV',
-    'DateNDF' => '25/02/12',
-    'MotifNDF' => 'Fidelisation',
-    'MontantPrevu' => '2,5',
-    'EtatNDF' => 'En cours',
-    'Commentaire' => 'En Attent',
-    'NbreNuiteesSiHotellerie' => 2,
-    'NbreRepasSiRestauration' => 5,
-    '$CoordonneesGPSDepartSiTransport' => '20 rue Pasteur Cadier',
-    'CoordonneesGPSArriveeSiTransport' => 'Paris 11 eme arrondissement',
-    'TypeDeTransport' => 'Voiture',
-    'DistanceSiTransport' => '600',
-    'Login' => 21,
-    'IdClient' => 3
-]);*/
 
-/*$notedefrais = new NoteDeFraisManager();*/
+//  $client = new ClientManager();
 
-            /*Ajout*/
+//             /*Ajout*/
 
-/*$notedefrais->create($notedefrais1);*/
+//  /*$client->create($client1);*/
 
+//         /*Modification*/
 
-            /*Modification*/
+// /*$client1->setPrenomClient("toto");
+// $client->update($client1);*/
 
-/*$notedefrais1->setMontantPrevu('1');
-$notedefrais->update($notedefrais1);*/
 
 
-            /*Suppression*/
-/*$notedefrais->delete($notedefrais1);*/
+//         /*Suppression*/
 
+// // $client->delete($client1);
 
 
 
+//                             /*TABLE notedefrais*/
 
+// /*$notedefrais1 = new NoteDeFrais([
+//     'IdNoteDeFrais' => 101,
+//     'IntituleNDF' => 'RDV',
+//     'DateNDF' => '25/02/12',
+//     'MotifNDF' => 'Fidelisation',
+//     'MontantPrevu' => '2,5',
+//     'EtatNDF' => 'En cours',
+//     'Commentaire' => 'En Attent',
+//     'NbreNuiteesSiHotellerie' => 2,
+//     'NbreRepasSiRestauration' => 5,
+//     '$CoordonneesGPSDepartSiTransport' => '20 rue Pasteur Cadier',
+//     'CoordonneesGPSArriveeSiTransport' => 'Paris 11 eme arrondissement',
+//     'TypeDeTransport' => 'Voiture',
+//     'DistanceSiTransport' => '600',
+//     'Login' => 21,
+//     'IdClient' => 3
+// ]);*/
 
-                                /*TABLE user*/
+// /*$notedefrais = new NoteDeFraisManager();*/
 
-/*$user1 = new User([
-    //'Login' => '194',
-    'NomReps' => 'Makarueta',
-    'PrenomReps' => 'Giovani',
-    'Adresse1Reps' => '20 rue Route de Tercis',
-    'Adresse2Reps' => '10 rue du Pasteur Cadier',
-    'CodePostalReps' => '64000',
-    'VilleReps' => 'Dax',
-    'EmailReps' => 'makaruetagiovani@gmail.com',
-    'TelephoneReps' => '06 76 14 98 17',
-    'Commentaires' => 'Nothing to add',
-    'DateEmbauche' => '2017-07-06',
-    'TypeDeDroits' => 'Consulter',
-    'MotDePasseUser' => 'Gre9',
-    'CategorieUser' => 'Commercial',
-    'IdType' => 21,
-]);*/
+//             /*Ajout*/
 
+// /*$notedefrais->create($notedefrais1);*/
 
-    $users=new UserManager();
 
+//             /*Modification*/
 
-//    *$users=new UserManager();*/
+// /*$notedefrais1->setMontantPrevu('1');
+// $notedefrais->update($notedefrais1);*/
 
 
+//             /*Suppression*/
+// /*$notedefrais->delete($notedefrais1);*/
 
 
 
-             /*Ajout*/
 
-    /*$users->create($user1);*/
 
 
+//                                 /*TABLE user*/
 
-            /*Modification*/
+// /*$user1 = new User([
+//     //'Login' => '194',
+//     'NomReps' => 'Makarueta',
+//     'PrenomReps' => 'Giovani',
+//     'Adresse1Reps' => '20 rue Route de Tercis',
+//     'Adresse2Reps' => '10 rue du Pasteur Cadier',
+//     'CodePostalReps' => '64000',
+//     'VilleReps' => 'Dax',
+//     'EmailReps' => 'makaruetagiovani@gmail.com',
+//     'TelephoneReps' => '06 76 14 98 17',
+//     'Commentaires' => 'Nothing to add',
+//     'DateEmbauche' => '2017-07-06',
+//     'TypeDeDroits' => 'Consulter',
+//     'MotDePasseUser' => 'Gre9',
+//     'CategorieUser' => 'Commercial',
+//     'IdType' => 21,
+// ]);*/
 
-   /* $user1->setVilleReps("");
-    $users->update($user1);*/
 
+//     $users=new UserManager();
 
-            /*Suppression*/
 
-    /*$users->delete($user1);*/
+// //    *$users=new UserManager();*/
 
 
 
-                                            /*TABLE tarifsremboursements*/
 
 
-/*$tarifsremboursement1 = new TarifsRemboursements([
-    'TypeDeFrais'=>53,
-    'MontantRemboursement' => '25,5',
-    'Unites' => 'KM'
-]);*/
+//              /*Ajout*/
 
-/*$tarifsremboursements = new TarifsRemboursementManager();*/
+//     /*$users->create($user1);*/
 
-                /*Ajout*/
-/*$tarifsremboursements->create($tarifsremboursement1);*/
 
-                /*Modification*/
-/*$tarifsremboursements->setMontantRemboursement('42');
-$tarifsremboursements->update($tarifsremboursement1);*/
 
+//             /*Modification*/
 
-                /*Suppression
-$tarifsremboursements->delete($tarifsremboursement1);*/
+//    /* $user1->setVilleReps("");
+//     $users->update($user1);*/
 
 
+//             /*Suppression*/
 
+//     /*$users->delete($user1);*/
 
-                                        /*TABLE justificatifs*/
 
-/*$justificatif1 = new Justificatif([
-    //'IdJustificatif' =>5,
-    'IntituleJustificatif' =>'Restauration',
-    'URLNomFichier' => '/justificatif/restauration/id=13',
-<<<<<<< HEAD
-    'MontantJustificatif' => '120'
-]);
 
-$justificatifs = new JustificatifManager();
-=======
-    'MontantJustificatif' => '120,5'
-]);*/
+//                                             /*TABLE tarifsremboursements*/
 
-/*$justificatifs = new JustificatifManager();*/
 
+// /*$tarifsremboursement1 = new TarifsRemboursements([
+//     'TypeDeFrais'=>53,
+//     'MontantRemboursement' => '25,5',
+//     'Unites' => 'KM'
+// ]);*/
 
-                  /*Ajout*/
-/*$justificatifs->create($justificatif1);*/
+// /*$tarifsremboursements = new TarifsRemboursementManager();*/
 
+//                 /*Ajout*/
+// /*$tarifsremboursements->create($tarifsremboursement1);*/
 
-                /*Modification*/
-/*$justificatif1->setMontantJustificatif('1');
-$justificatifs->update($justificatif1);*/
+//                 /*Modification*/
+// /*$tarifsremboursements->setMontantRemboursement('42');
+// $tarifsremboursements->update($tarifsremboursement1);*/
 
 
-                /*Suppression*/
-/*$justificatifs->delete($justificatif1);*/
+//                 /*Suppression
+// $tarifsremboursements->delete($tarifsremboursement1);*/
+
+
+
+
+//                                         /*TABLE justificatifs*/
+
+// /*$justificatif1 = new Justificatif([
+//     //'IdJustificatif' =>5,
+//     'IntituleJustificatif' =>'Restauration',
+//     'URLNomFichier' => '/justificatif/restauration/id=13',
+// <<<<<<< HEAD
+//     'MontantJustificatif' => '120'
+// ]);
+
+// $justificatifs = new JustificatifManager();
+// =======
+//     'MontantJustificatif' => '120,5'
+// ]);*/
+
+// /*$justificatifs = new JustificatifManager();*/
+
+
+//                   /*Ajout*/
+// /*$justificatifs->create($justificatif1);*/
+
+
+//                 /*Modification*/
+// /*$justificatif1->setMontantJustificatif('1');
+// $justificatifs->update($justificatif1);*/
+
+
+//                 /*Suppression*/
+// /*$justificatifs->delete($justificatif1);*/
 
 
 
