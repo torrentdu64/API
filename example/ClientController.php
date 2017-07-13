903ba7e3ebb4fd5b56e2deb1226a4af50b2f663f
MickaelDuprat <contact@mickaelduprat.fr>
2017-07-13 16:57:29 +0200

    commit rollback readall

--- example/ClientController.php
+++ example/ClientController.php
@@ -12,87 +12,41 @@ class CLientController
 	}
 
     /**
-     * Returns a JSON string object to the browser when hitting the root of the domain
-     *
-     * @url GET /
-     */
-    public function test()
-    {
-        return "Hello kikou";
-    }
-
-
-    /**
      * Logs in a user with the given username and password POSTed. Though true
      * REST doesn't believe in sessions, it is often desirable for an AJAX server.
      *
-     * @url GET /client
-     * 
+     * @url GET /client/$id
      */
   
 
-    public function getListClients()
+    public function getListClients($id)
     {       
-    	$listeClients[] = $this->clientManager->readAll();
+    	$listeClients = $this->clientManager->read($id);
         //var_dump($listeClients);
 
         $tabClients = [];
 
-    foreach ($listeClients as $key => $client) {
-                    $tabClients = ['IdClient' => $client->getIdClient(),
-                           'NomClient' => $client->getNomClient(),
-                           'PrenomClient' => $client->getPrenomClient(),
-                           'Adresse1Client' => $client->getAdresse1Client(),
-                           'Adresse2Client' => $client->getAdresse2Client(),
-                           'CodePostalClient' => $client->getCodePostalClient(),
-                           'VilleClient' => $client->getVilleClient(),
-                           'TelephoneBureauClient' => $client->getTelephoneBureauClient(),
-                           'TelephoneMobileClient' => $client->getTelephoneMobileClient(),
-                           'MailClient' => $client->getMailClient(),
-                           'BudgetMaxRemboursementClient' => $client->getBudgetMaxRemboursementClient(),
-                ];
-            //var_dump($listeClients);
-           //return  $tabClients['kikou'] = $tabClients;
+    // foreach ($listeClients as $key => $client) {
+    //                 $tabClients = ['IdClient' => $client->getIdClient(),
+    //                        'NomClient' => $client->getNomClient(),
+    //                        'PrenomClient' => $client->getPrenomClient(),
+    //                        'Adresse1Client' => $client->getAdresse1Client(),
+    //                        'Adresse2Client' => $client->getAdresse2Client(),
+    //                        'CodePostalClient' => $client->getCodePostalClient(),
+    //                        'VilleClient' => $client->getVilleClient(),
+    //                        'TelephoneBureauClient' => $client->getTelephoneBureauClient(),
+    //                        'TelephoneMobileClient' => $client->getTelephoneMobileClient(),
+    //                        'MailClient' => $client->getMailClient(),
+    //                        'BudgetMaxRemboursementClient' => $client->getBudgetMaxRemboursementClient(),
+    //             ];
+    //         //var_dump($listeClients);
+    //         $tabClients[] = $listeClients;
             
-        }
+    //     }
 
         // var_dump($tabClients);
-        return $tabClients;
-        
+        return $listeClients;
     }
-
-
-    
-
- // public function getOneClient($id){       
- //        $listeClients[] = $this->clientManager->read($id);
- //        //var_dump($listeClients);
-
- //        $tabClients = [];
-
- //    foreach ($listeClients as $key => $client) {
- //                    $tabClients = ['IdClient' => $client->getIdClient(),
- //                           'NomClient' => $client->getNomClient(),
- //                           'PrenomClient' => $client->getPrenomClient(),
- //                           'Adresse1Client' => $client->getAdresse1Client(),
- //                           'Adresse2Client' => $client->getAdresse2Client(),
- //                           'CodePostalClient' => $client->getCodePostalClient(),
- //                           'VilleClient' => $client->getVilleClient(),
- //                           'TelephoneBureauClient' => $client->getTelephoneBureauClient(),
- //                           'TelephoneMobileClient' => $client->getTelephoneMobileClient(),
- //                           'MailClient' => $client->getMailClient(),
- //                           'BudgetMaxRemboursementClient' => $client->getBudgetMaxRemboursementClient(),
- //                ];
- //            //var_dump($listeClients);
- //           //return  $tabClients['kikou'] = $tabClients;
-            
- //        }
-
- //        // var_dump($tabClients);
- //        return $tabClients;
-        
- //    }
-
 
 }
 
